048b45619c0455e09e6fdf81ff979b58950774d8
Abey Ashebir <161212522+Abey-Ashebir@users.noreply.github.com>
2025-08-15 17:07:09 +0300

    Update stats_pdf.blade.php
    simple view for pdf generated stats

--- resources/views/stats_pdf.blade.php
+++ resources/views/stats_pdf.blade.php
@@ -1,1 +1,23 @@
 
+<h1>Statistics Report</h1>
+<p>Total: {{ $stats['stats']['total'] }}</p>
+<p>Today: {{ $stats['stats']['today'] }}</p>
+<p>Top Source: {{ $stats['stats']['top_source']['signup_source'] ?? 'N/A' }}</p>
+
+<h3>Top Sources</h3>
+<table border="1" cellpadding="5">
+    <thead>
+        <tr>
+            <th>Source</th>
+            <th>Total</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($stats['top_sources'] as $source)
+            <tr>
+                <td>{{ $source['signup_source'] }}</td>
+                <td>{{ $source['total'] }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
