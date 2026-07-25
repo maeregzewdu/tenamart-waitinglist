6ed77b7aeca3accc5eafa579da31786bbf0d8d70
Maereg <maeregzewdu2020@gmail.com>
2026-07-26 01:43:19 +0300

    feat: fix favicon issue

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -37,11 +37,9 @@
         <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
 
         <!-- Favicons -->
-        <link rel="icon" href="/favicon.ico" type="image/x-icon">
-        <link rel="icon" type="image/png" href="{{ asset('favicon/favicon-96x96.png') }}" sizes="96x96" />
-        <link rel="icon" type="image/svg+xml" href="{{ asset('favicon/favicon.svg') }}" />
-        <link rel="shortcut icon" href="{{ asset('favicon/favicon.ico') }}" />
-        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}" />
+        <link rel="icon" href="/favicon.ico" sizes="any">
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
         <meta name="apple-mobile-web-app-title" content="TenaMart" />
         <link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}" />
         <meta name="csrf-token" content="{{ csrf_token() }}">
